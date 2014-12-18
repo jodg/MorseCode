173dc7d2832bd2855449aa7787e6df1c07312554
jodg <ding.jiao@gmail.com>
2014-12-18 15:17:41 +0800

    init

---
+++ MorseCode/MorseCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MorseCode
+//
+//  Created by jodg on 14/12/18.
+//  Copyright (c) 2014年 weibolabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MorseCode/MorseCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MorseCode
+//
+//  Created by jodg on 14/12/18.
+//  Copyright (c) 2014年 weibolabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
